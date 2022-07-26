9ba1367a305acd9c65300863e9a36c33ca30bd18
Juo-Tung Chen <jjcc0704@gmail.com>
2022-07-26 22:48:50 +0800

    - fix array stack overflow problem(segmentation fault)

--- week6/1_knapsack.cpp
+++ week6/1_knapsack.cpp
@@ -5,33 +5,36 @@ using std::vector;
 
 int optimal_weight(int W, const vector<int> &w) {
 	int n = w.size();     // n = number of items
-	int table[W+1][n+1];  //construct a table to record the results
-  	int current_weight = 0; //record the result
-	for (int i = 0; i <= W; i++){
-		for (int j = 0; j <= n; j++){
-			table[i][0] = 0;
-			table[0][j] = 0;	
-		}
-	}
+	
+	int** table = new int* [W+1];  //create a 2D array
+	for(int i = 0; i<=W ; i++)
+		table[i] = new int[n+1];  //construct a table to record the results
+  	int result_weight = 0; //record the result
+		
+	for (int i = 0; i <= n; i++){
+		for(int k = 0; k <= W; k++){
+			if(i == 0 || k == 0)
+				table[i][k] = 0;
 
-	for (int i = 1; i <= n; i++){
-		for(int k = 1; k <= W; k++){
-			table[k][i] = table[k][i-1];
-			if (w[i-1] <= k){
-				current_weight = table[k-w[i-1]][i-1] + w[i-1];
-				if (table[k][i] < current_weight)
-					table[k][i] = current_weight;
+			else if (w[i-1] <= k)
+				table[k][i] = std::max(table[k-w[i-1]][i-1]+w[i-1], table[k][i-1]);
+			else
+				table[k][i] = table[k][i-1];
+
+			std::cout<<table[k][i]<<' ';
 			}
+		std::cout<<std::endl;
 		}
-	}
-/*
-  	for (size_t i = 0; i < w.size(); ++i) {
-    		if (current_weight + w[i] <= W) {
-      			current_weight += w[i];
-    		}
-  	}
-*/
-  	return table[W][n];
+	
+	
+	result_weight = table[W][n];
+	
+	// clear the dynamic array
+	for(int i = 0; i<=W; i++)
+		delete[] table[i];
+	delete[] table;
+
+  	return result_weight;
 }
 
 int main() {
